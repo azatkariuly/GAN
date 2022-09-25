b4da885c9bb5865c6d34c8bec1482616b99c3ece
azatkariuly <azat.kariuly@gmail.com>
2022-09-25 14:31:58 +0900

    initial commit

--- preprocess.py
+++ preprocess.py
@@ -2,8 +2,10 @@ import torch
 import torchvision.transforms as transforms
 import random
 
-def get_transform(name='cifar10'):
+def get_transform(name='cifar10', img_size=None):
     if name=='cifar10':
+        image_size = img_size or 64
+        print('ailsdngoiamnsdoigmasdoimoi md:', image_size)
         return transforms.Compose([transforms.Resize(image_size),
                                    transforms.CenterCrop(image_size),
                                    transforms.ToTensor(),
