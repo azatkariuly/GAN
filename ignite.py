3f6ece200dfaec7e719259630d73aaff9aef8090
azatkariuly <azat.kariuly@gmail.com>
2022-09-26 15:27:29 +0900

    initial commit

---
+++ ignite.py
@@ -0,0 +1,99 @@
+import os
+import logging
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+from torchsummary import summary
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+
+from ignite.engine import Engine, Events
+import ignite.distributed as idist
+
+from torchvision.datasets import ImageFolder
+
+ignite.utils.manual_seed(999)
+ignite.utils.setup_logger(name="ignite.distributed.auto.auto_dataloader", level=logging.WARNING)
+ignite.utils.setup_logger(name="ignite.distributed.launcher.Parallel", level=logging.WARNING)
+
+# Dataset and Transformation
+
+image_size = 64
+
+data_transform = transforms.Compose(
+    [
+        transforms.Resize(image_size),
+        transforms.CenterCrop(image_size),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]
+)
+
+train_dataset = ImageFolder(root="./data", transform=data_transform)
+test_dataset = torch.utils.data.Subset(train_dataset, torch.arange(3000))
+
+# DataLoading
+
+batch_size = 128
+
+train_dataloader = idist.auto_dataloader(
+    train_dataset,
+    batch_size=batch_size,
+    num_workers=2,
+    shuffle=True,
+    drop_last=True,
+)
+
+test_dataloader = idist.auto_dataloader(
+    test_dataset,
+    batch_size=batch_size,
+    num_workers=2,
+    shuffle=False,
+    drop_last=True,
+)
+
+# Generator
+
+latent_dim = 100
+
+class Generator3x64x64(nn.Module):
+    def __init__(self, latent_dim):
+        super(Generator3x64x64, self).__init__()
+        self.model = nn.Sequential(
+            nn.ConvTranspose2d(latent_dim, 512, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            # state size. 512 x 4 x 4
+            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            # state size. 256 x 8 x 8
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            # state size. 128 x 16 x 16
+            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            # state size. 64 x 32 x 32
+            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # final state size. 3 x 64 x 64
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+netG = idist.auto_model(Generator3x64x64(latent_dim))
+
+# Note that the model is automatically moved to the best device detected by idist.
+idist.device()
+
+summary(netG, (latent_dim, 1, 1))
