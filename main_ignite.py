83a38cb1b0e4867bc92ee41b884462328d50d880
azatkariuly <azat.kariuly@gmail.com>
2022-09-26 16:17:35 +0900

    initial commit

--- main_ignite.py
+++ main_ignite.py
@@ -27,64 +27,6 @@ ignite.utils.setup_logger(name="ignite.distributed.launcher.Parallel", level=log
 
 # Dataset and Transformation
 
-print('Start downloading..')
-
-def get_dataloaders_celeba(batch_size, num_workers=0,
-                           train_transforms=None,
-                           test_transforms=None,
-                           download=True):
-
-    if train_transforms is None:
-        train_transforms = transforms.ToTensor()
-
-    if test_transforms is None:
-        test_transforms = transforms.ToTensor()
-
-    train_dataset = datasets.CelebA(root='../data',
-                                    split='train',
-                                    transform=train_transforms,
-                                    download=download)
-
-    valid_dataset = datasets.CelebA(root='../data',
-                                    split='valid',
-                                    transform=test_transforms)
-
-    test_dataset = datasets.CelebA(root='../data',
-                                   split='test',
-                                   transform=test_transforms)
-
-
-    train_loader = DataLoader(dataset=train_dataset,
-                              batch_size=batch_size,
-                              num_workers=num_workers,
-                              shuffle=True)
-
-    valid_loader = DataLoader(dataset=test_dataset,
-                             batch_size=batch_size,
-                             num_workers=num_workers,
-                             shuffle=False)
-
-    test_loader = DataLoader(dataset=test_dataset,
-                             batch_size=batch_size,
-                             num_workers=num_workers,
-                             shuffle=False)
-
-    return train_loader, valid_loader, test_loader
-
-custom_transforms = torchvision.transforms.Compose([
-    torchvision.transforms.CenterCrop(64),
-    torchvision.transforms.Resize(64),
-    torchvision.transforms.ToTensor(),
-    torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
-
-train_loader, valid_loader, test_loader = get_dataloaders_celeba(
-    batch_size=128,
-    train_transforms=custom_transforms,
-    test_transforms=custom_transforms,
-    num_workers=4)
-
-'''
 image_size = 64
 
 data_transform = transforms.Compose(
@@ -158,4 +100,214 @@ netG = idist.auto_model(Generator3x64x64(latent_dim))
 idist.device()
 
 summary(netG, (latent_dim, 1, 1))
-'''
+
+# Discriminator
+
+class Discriminator3x64x64(nn.Module):
+    def __init__(self):
+        super(Discriminator3x64x64, self).__init__()
+        self.model = nn.Sequential(
+            # input is 3 x 64 x 64
+            nn.Conv2d(3, 64, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. 64 x 32 x 32
+            nn.Conv2d(64, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. 128 x 16 x 16
+            nn.Conv2d(128, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. 256 x 8 x 8
+            nn.Conv2d(256, 512, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. 512 x 4 x 4
+            nn.Conv2d(512, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+netD = idist.auto_model(Discriminator3x64x64())
+summary(netD, (3, 64, 64))
+
+# Optimizers
+
+criterion = nn.BCELoss()
+
+fixed_noise = torch.randn(64, latent_dim, 1, 1, device=idist.device())
+
+optimizerD = idist.auto_optim(
+    optim.Adam(netD.parameters(), lr=0.0002, betas=(0.5, 0.999))
+)
+
+optimizerG = idist.auto_optim(
+    optim.Adam(netG.parameters(), lr=0.0002, betas=(0.5, 0.999))
+)
+
+# Training
+
+real_label = 1
+fake_label = 0
+
+def training_step(engine, data):
+    # Set the models for training
+    netG.train()
+    netD.train()
+
+    ############################
+    # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+    ###########################
+    ## Train with all-real batch
+    netD.zero_grad()
+    # Format batch
+    real = data[0].to(idist.device())
+    b_size = real.size(0)
+    label = torch.full((b_size,), real_label, dtype=torch.float, device=idist.device())
+    # Forward pass real batch through D
+    output1 = netD(real).view(-1)
+    # Calculate loss on all-real batch
+    errD_real = criterion(output1, label)
+    # Calculate gradients for D in backward pass
+    errD_real.backward()
+
+    ## Train with all-fake batch
+    # Generate batch of latent vectors
+    noise = torch.randn(b_size, latent_dim, 1, 1, device=idist.device())
+    # Generate fake image batch with G
+    fake = netG(noise)
+    label.fill_(fake_label)
+    # Classify all fake batch with D
+    output2 = netD(fake.detach()).view(-1)
+    # Calculate D's loss on the all-fake batch
+    errD_fake = criterion(output2, label)
+    # Calculate the gradients for this batch, accumulated (summed) with previous gradients
+    errD_fake.backward()
+    # Compute error of D as sum over the fake and the real batches
+    errD = errD_real + errD_fake
+    # Update D
+    optimizerD.step()
+
+    ############################
+    # (2) Update G network: maximize log(D(G(z)))
+    ###########################
+    netG.zero_grad()
+    label.fill_(real_label)  # fake labels are real for generator cost
+    # Since we just updated D, perform another forward pass of all-fake batch through D
+    output3 = netD(fake).view(-1)
+    # Calculate G's loss based on this output
+    errG = criterion(output3, label)
+    # Calculate gradients for G
+    errG.backward()
+    # Update G
+    optimizerG.step()
+
+    return {
+        "Loss_G" : errG.item(),
+        "Loss_D" : errD.item(),
+        "D_x": output1.mean().item(),
+        "D_G_z1": output2.mean().item(),
+        "D_G_z2": output3.mean().item(),
+    }
+
+trainer = Engine(training_step)
+
+def initialize_fn(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+@trainer.on(Events.STARTED)
+def init_weights():
+    netD.apply(initialize_fn)
+    netG.apply(initialize_fn)
+
+G_losses = []
+D_losses = []
+
+@trainer.on(Events.ITERATION_COMPLETED)
+def store_losses(engine):
+    o = engine.state.output
+    G_losses.append(o["Loss_G"])
+    D_losses.append(o["Loss_D"])
+
+img_list = []
+
+@trainer.on(Events.ITERATION_COMPLETED(every=500))
+def store_images(engine):
+    with torch.no_grad():
+        fake = netG(fixed_noise).cpu()
+    img_list.append(fake)
+
+# FID and IS scores
+from ignite.metrics import FID, InceptionScore
+
+fid_metric = FID(device=idist.device())
+is_metric = InceptionScore(device=idist.device(), output_transform=lambda x: x[0])
+
+# Evaluators
+
+import PIL.Image as Image
+
+def interpolate(batch):
+    arr = []
+    for img in batch:
+        pil_img = transforms.ToPILImage()(img)
+        resized_img = pil_img.resize((299,299), Image.BILINEAR)
+        arr.append(transforms.ToTensor()(resized_img))
+    return torch.stack(arr)
+
+def evaluation_step(engine, batch):
+    with torch.no_grad():
+        noise = torch.randn(batch_size, latent_dim, 1, 1, device=idist.device())
+        netG.eval()
+        fake_batch = netG(noise)
+        fake = interpolate(fake_batch)
+        real = interpolate(batch[0])
+        return fake, real
+
+evaluator = Engine(evaluation_step)
+fid_metric.attach(evaluator, "fid")
+is_metric.attach(evaluator, "is")
+
+fid_values = []
+is_values = []
+
+
+@trainer.on(Events.EPOCH_COMPLETED)
+def log_training_results(engine):
+    evaluator.run(test_dataloader,max_epochs=1)
+    metrics = evaluator.state.metrics
+    fid_score = metrics['fid']
+    is_score = metrics['is']
+    fid_values.append(fid_score)
+    is_values.append(is_score)
+    print(f"Epoch [{engine.state.epoch}/5] Metric Scores")
+    print(f"*   FID : {fid_score:4f}")
+    print(f"*    IS : {is_score:4f}")
+
+from ignite.metrics import RunningAverage
+
+RunningAverage(output_transform=lambda x: x["Loss_G"]).attach(trainer, 'Loss_G')
+RunningAverage(output_transform=lambda x: x["Loss_D"]).attach(trainer, 'Loss_D')
+
+# Progress bar
+
+from ignite.contrib.handlers import ProgressBar
+
+ProgressBar().attach(trainer, metric_names=['Loss_G','Loss_D'])
+ProgressBar().attach(evaluator)
+
+# Training
+
+def training(*args):
+    trainer.run(train_dataloader, max_epochs=5)
+
+with idist.Parallel(backend='nccl') as parallel:
+    parallel.run(training)
