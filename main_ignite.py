202da65f1f8596476964553e56bfdaf3c29cb6eb
azatkariuly <azat.kariuly@gmail.com>
2022-09-27 15:33:03 +0900

    initial commit

--- main_ignite.py
+++ main_ignite.py
@@ -14,6 +14,7 @@ import torch.optim as optim
 
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
+import torchvision.models as models
 from torchvision import datasets
 
 from ignite.engine import Engine, Events
