decb572fed6ea679f1d90e66c58ff7ae9164d27d
azatkariuly <azat.kariuly@gmail.com>
2022-09-27 16:46:07 +0900

    initial commit

--- main2.py
+++ main2.py
@@ -1,466 +1,95 @@
-import os
-import logging
-import matplotlib.pyplot as plt
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch.nn import functional as F
+import torch.utils.data
+
+from torchvision.models.inception import inception_v3
 
 import numpy as np
-
-import torchvision
-import ignite
-from torchsummary import summary
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-
-import torchvision.transforms as transforms
-import torchvision.utils as vutils
-import torchvision.models as models
-from torchvision import datasets
-
-from ignite.engine import Engine, Events
-import ignite.distributed as idist
-
-from torchvision.datasets import ImageFolder
-
-# ignite.utils.manual_seed(999)
-# ignite.utils.setup_logger(name="ignite.distributed.auto.auto_dataloader", level=logging.WARNING)
-# ignite.utils.setup_logger(name="ignite.distributed.launcher.Parallel", level=logging.WARNING)
-
-# Dataset and Transformation
-
-image_size = 64
-batch_size = 128
-ngpu = 1
-
-#normalizing input between -1 and 1
-transform = transforms.Compose([transforms.Resize(image_size),
-                              transforms.CenterCrop(image_size),
-                              transforms.ToTensor(),
-                              transforms.Normalize((0,0,0), (1,1,1)),])
-
-
-train_dataset = datasets.CIFAR10(root='../data/CIFAR10', train=True,
-                                        download=True, transform=transform)
-test_dataset = datasets.CIFAR10(root='../data/CIFAR10', train=False,
-                                        download=True, transform=transform)
-
-# train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
-#                                           shuffle=True, num_workers=2)
-# test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
-#                                           shuffle=False, num_workers=2)
-
-# Decide which device we want to run on
-device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
-
-##################
-
-# train_dataset = ImageFolder(root="../data", transform=data_transform)
-# test_dataset = torch.utils.data.Subset(train_dataset, torch.arange(3000))
-
-# DataLoading
-
-train_dataloader = idist.auto_dataloader(
-    train_dataset,
-    batch_size=batch_size,
-    num_workers=2,
-    shuffle=True,
-    drop_last=True,
-)
-
-test_dataloader = idist.auto_dataloader(
-    test_dataset,
-    batch_size=batch_size,
-    num_workers=2,
-    shuffle=False,
-    drop_last=True,
-)
-
-
-# Generator
-
-latent_dim = 100
-
-class Generator(nn.Module):
-    def __init__(self, latent_dim=100):
-        super(Generator, self).__init__()
-        self.main = nn.Sequential(
-            # input is Z, going into a convolution
-            nn.ConvTranspose2d(latent_dim, 64 * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(64 * 8),
-            nn.ReLU(True),
-            # state size. (ngf*8) x 4 x 4
-            nn.ConvTranspose2d(64 * 8, 64 * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64 * 4),
-            nn.ReLU(True),
-            # state size. (ngf*4) x 8 x 8
-            nn.ConvTranspose2d( 64 * 4, 64 * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64 * 2),
-            nn.ReLU(True),
-            # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d( 64 * 2, 64, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(True),
-            # state size. (ngf) x 32 x 32
-            nn.ConvTranspose2d( 64, 3, 4, 2, 1, bias=False),
-            nn.Tanh()
-            # state size. (nc) x 64 x 64
-        )
-
-    def forward(self, input):
-        return self.main(input)
-
-netG = idist.auto_model(Generator(latent_dim))
-
-# Note that the model is automatically moved to the best device detected by idist.
-idist.device()
-
-summary(netG, (latent_dim, 1, 1))
-
-
-# Discriminator
-
-class Discriminator(nn.Module):
-    def __init__(self):
-        super(Discriminator, self).__init__()
-        self.main = nn.Sequential(
-            # input is (nc) x 64 x 64
-            nn.Conv2d(3, 64, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf) x 32 x 32
-            nn.Conv2d(64, 64 * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64 * 2),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 16 x 16
-            nn.Conv2d(64 * 2, 64 * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64 * 4),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(64 * 4, 64 * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64 * 8),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(64 * 8, 1, 4, 1, 0, bias=False),
-            nn.Sigmoid()
-        )
-
-    def forward(self, input):
-        return self.main(input)
-
-netD = idist.auto_model(Discriminator())
-summary(netD, (3, 64, 64))
-
-
-# Optimizers
-
-criterion = nn.BCELoss()
-
-fixed_noise = torch.randn(64, latent_dim, 1, 1, device=idist.device())
-
-optimizerD = idist.auto_optim(
-    optim.Adam(netD.parameters(), lr=0.0002, betas=(0.5, 0.999))
-)
-
-optimizerG = idist.auto_optim(
-    optim.Adam(netG.parameters(), lr=0.0002, betas=(0.5, 0.999))
-)
-
-# Training
-
-real_label = 1
-fake_label = 0
-
-def training_step(engine, data):
-    # Set the models for training
-    netG.train()
-    netD.train()
-
-    ############################
-    # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-    ###########################
-    ## Train with all-real batch
-    netD.zero_grad()
-    # Format batch
-    real = data[0].to(idist.device())
-    b_size = real.size(0)
-    label = torch.full((b_size,), real_label, dtype=torch.float, device=idist.device())
-    # Forward pass real batch through D
-    output1 = netD(real).view(-1)
-    # Calculate loss on all-real batch
-    errD_real = criterion(output1, label)
-    # Calculate gradients for D in backward pass
-    errD_real.backward()
-
-    ## Train with all-fake batch
-    # Generate batch of latent vectors
-    noise = torch.randn(b_size, latent_dim, 1, 1, device=idist.device())
-    # Generate fake image batch with G
-    fake = netG(noise)
-    label.fill_(fake_label)
-    # Classify all fake batch with D
-    output2 = netD(fake.detach()).view(-1)
-    # Calculate D's loss on the all-fake batch
-    errD_fake = criterion(output2, label)
-    # Calculate the gradients for this batch, accumulated (summed) with previous gradients
-    errD_fake.backward()
-    # Compute error of D as sum over the fake and the real batches
-    errD = errD_real + errD_fake
-    # Update D
-    optimizerD.step()
-
-    ############################
-    # (2) Update G network: maximize log(D(G(z)))
-    ###########################
-    netG.zero_grad()
-    label.fill_(real_label)  # fake labels are real for generator cost
-    # Since we just updated D, perform another forward pass of all-fake batch through D
-    output3 = netD(fake).view(-1)
-    # Calculate G's loss based on this output
-    errG = criterion(output3, label)
-    # Calculate gradients for G
-    errG.backward()
-    # Update G
-    optimizerG.step()
-
-    return {
-        "Loss_G" : errG.item(),
-        "Loss_D" : errD.item(),
-        "D_x": output1.mean().item(),
-        "D_G_z1": output2.mean().item(),
-        "D_G_z2": output3.mean().item(),
-    }
-
-trainer = Engine(training_step)
-
-def initialize_fn(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
-
-@trainer.on(Events.STARTED)
-def init_weights():
-    netD.apply(initialize_fn)
-    netG.apply(initialize_fn)
-
-G_losses = []
-D_losses = []
-
-@trainer.on(Events.ITERATION_COMPLETED)
-def store_losses(engine):
-    o = engine.state.output
-    G_losses.append(o["Loss_G"])
-    D_losses.append(o["Loss_D"])
-
-img_list = []
-
-@trainer.on(Events.ITERATION_COMPLETED(every=500))
-def store_images(engine):
-    with torch.no_grad():
-        fake = netG(fixed_noise).cpu()
-    img_list.append(fake)
-
-# FID and IS scores
-from ignite.metrics import FID, InceptionScore
-
-fid_metric = FID(device=idist.device())
-is_metric = InceptionScore(device=idist.device(), output_transform=lambda x: x[0])
-
-# wrapper class as feature_extractor
-class WrapperInceptionV3(nn.Module):
-
-    def __init__(self, fid_incv3):
-        super().__init__()
-        self.fid_incv3 = fid_incv3
-
-    @torch.no_grad()
-    def forward(self, x):
-        y = self.fid_incv3(x)
-        y = y[0]
-        y = y[:, :, 0, 0]
-        return y
-
-class InceptionV3(nn.Module):
-    """Pretrained InceptionV3 network returning feature maps"""
-
-    # Index of default block of inception to return,
-    # corresponds to output of final average pooling
-    DEFAULT_BLOCK_INDEX = 3
-
-    # Maps feature dimensionality to their output blocks indices
-    BLOCK_INDEX_BY_DIM = {
-        64: 0,   # First max pooling features
-        192: 1,  # Second max pooling featurs
-        768: 2,  # Pre-aux classifier features
-        2048: 3  # Final average pooling features
-    }
-
-    def __init__(self,
-                 output_blocks=[DEFAULT_BLOCK_INDEX],
-                 resize_input=True,
-                 normalize_input=True,
-                 requires_grad=False):
-
-        super(InceptionV3, self).__init__()
-
-        self.resize_input = resize_input
-        self.normalize_input = normalize_input
-        self.output_blocks = sorted(output_blocks)
-        self.last_needed_block = max(output_blocks)
-
-        assert self.last_needed_block <= 3, \
-            'Last possible output block index is 3'
-
-        self.blocks = nn.ModuleList()
-
-
-        inception = models.inception_v3(pretrained=True)
-
-        # Block 0: input to maxpool1
-        block0 = [
-            inception.Conv2d_1a_3x3,
-            inception.Conv2d_2a_3x3,
-            inception.Conv2d_2b_3x3,
-            nn.MaxPool2d(kernel_size=3, stride=2)
-        ]
-        self.blocks.append(nn.Sequential(*block0))
-
-        # Block 1: maxpool1 to maxpool2
-        if self.last_needed_block >= 1:
-            block1 = [
-                inception.Conv2d_3b_1x1,
-                inception.Conv2d_4a_3x3,
-                nn.MaxPool2d(kernel_size=3, stride=2)
-            ]
-            self.blocks.append(nn.Sequential(*block1))
-
-        # Block 2: maxpool2 to aux classifier
-        if self.last_needed_block >= 2:
-            block2 = [
-                inception.Mixed_5b,
-                inception.Mixed_5c,
-                inception.Mixed_5d,
-                inception.Mixed_6a,
-                inception.Mixed_6b,
-                inception.Mixed_6c,
-                inception.Mixed_6d,
-                inception.Mixed_6e,
-            ]
-            self.blocks.append(nn.Sequential(*block2))
-
-        # Block 3: aux classifier to final avgpool
-        if self.last_needed_block >= 3:
-            block3 = [
-                inception.Mixed_7a,
-                inception.Mixed_7b,
-                inception.Mixed_7c,
-                nn.AdaptiveAvgPool2d(output_size=(1, 1))
-            ]
-            self.blocks.append(nn.Sequential(*block3))
-
-        for param in self.parameters():
-            param.requires_grad = requires_grad
-
-    def forward(self, inp):
-        outp = []
-        x = inp
-
-        if self.resize_input:
-            x = F.interpolate(x,
-                              size=(299, 299),
-                              mode='bilinear',
-                              align_corners=False)
-
-        if self.normalize_input:
-            x = 2 * x - 1  # Scale from range (0, 1) to range (-1, 1)
-
-        for idx, block in enumerate(self.blocks):
-            x = block(x)
-            if idx in self.output_blocks:
-                outp.append(x)
-
-            if idx == self.last_needed_block:
-                break
-
-        return outp
-
-# pytorch_fid model
-dims = 2048
-block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[dims]
-model = InceptionV3([block_idx]).to(idist.device())
-
-# wrapper model to pytorch_fid model
-wrapper_model = WrapperInceptionV3(model)
-wrapper_model.eval();
-
-# comparable metric
-pytorch_fid_metric = FID(num_features=dims, feature_extractor=wrapper_model)
-
-# Evaluators
-
-import PIL.Image as Image
-
-def interpolate(batch):
-    arr = []
-    for img in batch:
-        pil_img = transforms.ToPILImage()(img)
-        resized_img = pil_img.resize((299,299), Image.BILINEAR)
-        arr.append(transforms.ToTensor()(resized_img))
-    return torch.stack(arr)
-
-def evaluation_step(engine, batch):
-    with torch.no_grad():
-        noise = torch.randn(batch_size, latent_dim, 1, 1, device=idist.device())
-        netG.eval()
-        fake_batch = netG(noise)
-        fake = interpolate(fake_batch)
-        real = interpolate(batch[0])
-        return fake, real
-
-evaluator = Engine(evaluation_step)
-fid_metric.attach(evaluator, "fid")
-is_metric.attach(evaluator, "is")
-pytorch_fid_metric.attach(evaluator, 'pytorch_fid')
-
-fid_values = []
-is_values = []
-pytorch_fid = []
-
-
-@trainer.on(Events.EPOCH_COMPLETED)
-def log_training_results(engine):
-    evaluator.run(test_dataloader,max_epochs=1)
-    metrics = evaluator.state.metrics
-    fid_score = metrics['fid']
-    is_score = metrics['is']
-    pytorch_fid_score = metrics['pytorch_fid']
-    fid_values.append(fid_score)
-    is_values.append(is_score)
-    pytorch_fid.append(pytorch_fid_score)
-
-    print(f"Epoch [{engine.state.epoch}/5] Metric Scores")
-    print(f"*   FID : {fid_score:4f}")
-    print(f"*    IS : {is_score:4f}")
-    print(f"*    PFID : {pytorch_fid_score:4f}")
-
-from ignite.metrics import RunningAverage
-
-RunningAverage(output_transform=lambda x: x["Loss_G"]).attach(trainer, 'Loss_G')
-RunningAverage(output_transform=lambda x: x["Loss_D"]).attach(trainer, 'Loss_D')
-
-# Progress bar
-
-from ignite.contrib.handlers import ProgressBar
-
-ProgressBar().attach(trainer, metric_names=['Loss_G','Loss_D'])
-ProgressBar().attach(evaluator)
-
-# Training
-
-def training(*args):
-    trainer.run(train_dataloader, max_epochs=5)
-
-with idist.Parallel(backend='nccl') as parallel:
-    parallel.run(training)
+from scipy.stats import entropy
+
+def inception_score(imgs, cuda=True, batch_size=32, resize=False, splits=1):
+    """Computes the inception score of the generated images imgs
+
+    imgs -- Torch dataset of (3xHxW) numpy images normalized in the range [-1, 1]
+    cuda -- whether or not to run on GPU
+    batch_size -- batch size for feeding into Inception v3
+    splits -- number of splits
+    """
+    N = len(imgs)
+
+    assert batch_size > 0
+    assert N > batch_size
+
+    # Set up dtype
+    if cuda:
+        dtype = torch.cuda.FloatTensor
+    else:
+        if torch.cuda.is_available():
+            print("WARNING: You have a CUDA device, so you should probably set cuda=True")
+        dtype = torch.FloatTensor
+
+    # Set up dataloader
+    dataloader = torch.utils.data.DataLoader(imgs, batch_size=batch_size)
+
+    # Load inception model
+    inception_model = inception_v3(pretrained=True, transform_input=False).type(dtype)
+    inception_model.eval();
+    up = nn.Upsample(size=(299, 299), mode='bilinear').type(dtype)
+    def get_pred(x):
+        if resize:
+            x = up(x)
+        x = inception_model(x)
+        return F.softmax(x).data.cpu().numpy()
+
+    # Get predictions
+    preds = np.zeros((N, 1000))
+
+    for i, batch in enumerate(dataloader, 0):
+        batch = batch.type(dtype)
+        batchv = Variable(batch)
+        batch_size_i = batch.size()[0]
+
+        preds[i*batch_size:i*batch_size + batch_size_i] = get_pred(batchv)
+
+    # Now compute the mean kl-div
+    split_scores = []
+
+    for k in range(splits):
+        part = preds[k * (N // splits): (k+1) * (N // splits), :]
+        py = np.mean(part, axis=0)
+        scores = []
+        for i in range(part.shape[0]):
+            pyx = part[i, :]
+            scores.append(entropy(pyx, py))
+        split_scores.append(np.exp(np.mean(scores)))
+
+    return np.mean(split_scores), np.std(split_scores)
+
+if __name__ == '__main__':
+    class IgnoreLabelDataset(torch.utils.data.Dataset):
+        def __init__(self, orig):
+            self.orig = orig
+
+        def __getitem__(self, index):
+            return self.orig[index][0]
+
+        def __len__(self):
+            return len(self.orig)
+
+    import torchvision.datasets as dset
+    import torchvision.transforms as transforms
+
+    cifar = dset.CIFAR10(root='data/', download=True,
+                             transform=transforms.Compose([
+                                 transforms.Scale(32),
+                                 transforms.ToTensor(),
+                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                             ])
+    )
+
+    IgnoreLabelDataset(cifar)
+
+    print ("Calculating Inception Score...")
+    print (inception_score(IgnoreLabelDataset(cifar), cuda=True, batch_size=32, resize=True, splits=10))
