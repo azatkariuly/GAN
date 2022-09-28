a408e109d2ed08813b363851f39fd0554ff0c7b1
azatkariuly <azat.kariuly@gmail.com>
2022-09-29 02:20:48 +0900

    initial commit

--- utils.py
+++ utils.py
@@ -4,6 +4,15 @@ import torchvision.models as models
 import torch.nn.functional as F
 from scipy import linalg
 
+# custom weights initialization called on netG and netD
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
 class InceptionV3(nn.Module):
     """Pretrained InceptionV3 network returning feature maps"""
 
