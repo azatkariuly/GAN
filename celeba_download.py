47bd38eac38da4c33fcaff1c03d4a8f694359d63
azatkariuly <azat.kariuly@gmail.com>
2022-09-28 22:30:48 +0900

    initial commit

--- celeba_download.py
+++ celeba_download.py
@@ -6,6 +6,7 @@ import torch
 import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 from torchvision import transforms
 from torchvision import datasets
@@ -64,16 +65,16 @@ def get_dataloaders_celeba(batch_size, num_workers=0,
     if test_transforms is None:
         test_transforms = transforms.ToTensor()
 
-    train_dataset = datasets.CelebA(root='../data',
+    train_dataset = datasets.CelebA(root='data',
                                     split='train',
                                     transform=train_transforms,
                                     download=download)
 
-    valid_dataset = datasets.CelebA(root='../data',
+    valid_dataset = datasets.CelebA(root='data',
                                     split='valid',
                                     transform=test_transforms)
 
-    test_dataset = datasets.CelebA(root='../data',
+    test_dataset = datasets.CelebA(root='data',
                                    split='test',
                                    transform=test_transforms)
 
@@ -95,16 +96,288 @@ def get_dataloaders_celeba(batch_size, num_workers=0,
 
     return train_loader, valid_loader, test_loader
 
+def train_gan_v1(num_epochs, model, optimizer_gen, optimizer_discr,
+                 latent_dim, device, train_loader, loss_fn=None,
+                 logging_interval=100,
+                 save_model=None):
+
+    log_dict = {'train_generator_loss_per_batch': [],
+                'train_discriminator_loss_per_batch': [],
+                'train_discriminator_real_acc_per_batch': [],
+                'train_discriminator_fake_acc_per_batch': [],
+                'images_from_noise_per_epoch': []}
+
+    if loss_fn is None:
+        loss_fn = F.binary_cross_entropy_with_logits
+
+    # Batch of latent (noise) vectors for
+    # evaluating / visualizing the training progress
+    # of the generator
+    fixed_noise = torch.randn(64, latent_dim, 1, 1, device='cuda') # format NCHW
+
+    start_time = time.time()
+    for epoch in range(num_epochs):
+
+        model.train()
+        for batch_idx, (features, _) in enumerate(train_loader):
+
+            batch_size = features.size(0)
+
+            # real images
+            real_images = features.to(device)
+            real_labels = torch.ones(batch_size, device=device) # real label = 1
+
+            # generated (fake) images
+            noise = torch.randn(batch_size, latent_dim, 1, 1, device=device)  # format NCHW
+            fake_images = model.generator_forward(noise)
+            fake_labels = torch.zeros(batch_size, device=device) # fake label = 0
+            flipped_fake_labels = real_labels # here, fake label = 1
+
+            # --------------------------
+            # Train Discriminator
+            # --------------------------
+
+            optimizer_discr.zero_grad()
+
+            # get discriminator loss on real images
+            discr_pred_real = model.discriminator_forward(real_images).view(-1) # Nx1 -> N
+            real_loss = loss_fn(discr_pred_real, real_labels)
+            # real_loss.backward()
+
+            # get discriminator loss on fake images
+            discr_pred_fake = model.discriminator_forward(fake_images.detach()).view(-1)
+            fake_loss = loss_fn(discr_pred_fake, fake_labels)
+            # fake_loss.backward()
+
+            # combined loss
+            discr_loss = 0.5*(real_loss + fake_loss)
+            discr_loss.backward()
+
+            optimizer_discr.step()
+
+            # --------------------------
+            # Train Generator
+            # --------------------------
+
+            optimizer_gen.zero_grad()
+
+            # get discriminator loss on fake images with flipped labels
+            discr_pred_fake = model.discriminator_forward(fake_images).view(-1)
+            gener_loss = loss_fn(discr_pred_fake, flipped_fake_labels)
+            gener_loss.backward()
+
+            optimizer_gen.step()
+
+            # --------------------------
+            # Logging
+            # --------------------------
+            log_dict['train_generator_loss_per_batch'].append(gener_loss.item())
+            log_dict['train_discriminator_loss_per_batch'].append(discr_loss.item())
+
+            predicted_labels_real = torch.where(discr_pred_real.detach() > 0., 1., 0.)
+            predicted_labels_fake = torch.where(discr_pred_fake.detach() > 0., 1., 0.)
+            acc_real = (predicted_labels_real == real_labels).float().mean()*100.
+            acc_fake = (predicted_labels_fake == fake_labels).float().mean()*100.
+            log_dict['train_discriminator_real_acc_per_batch'].append(acc_real.item())
+            log_dict['train_discriminator_fake_acc_per_batch'].append(acc_fake.item())
+
+            if not batch_idx % logging_interval:
+                print('Epoch: %03d/%03d | Batch %03d/%03d | Gen/Dis Loss: %.4f/%.4f'
+                       % (epoch+1, num_epochs, batch_idx,
+                          len(train_loader), gener_loss.item(), discr_loss.item()))
+
+        ### Save images for evaluation
+        with torch.no_grad():
+            fake_images = model.generator_forward(fixed_noise).detach().cpu()
+            log_dict['images_from_noise_per_epoch'].append(
+                torchvision.utils.make_grid(fake_images, padding=2, normalize=True))
+
+
+        print('Time elapsed: %.2f min' % ((time.time() - start_time)/60))
+
+    print('Total Training Time: %.2f min' % ((time.time() - start_time)/60))
+
+    if save_model is not None:
+        torch.save(model.state_dict(), save_model)
+
+    return log_dict
+
+def set_deterministic():
+    if torch.cuda.is_available():
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+    torch.set_deterministic(True)
+
+
+def set_all_seeds(seed):
+    os.environ["PL_GLOBAL_SEED"] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+def plot_multiple_training_losses(losses_list, num_epochs,
+                                  averaging_iterations=100, custom_labels_list=None):
+
+    for i,_ in enumerate(losses_list):
+        if not len(losses_list[i]) == len(losses_list[0]):
+            raise ValueError('All loss tensors need to have the same number of elements.')
+
+    if custom_labels_list is None:
+        custom_labels_list = [str(i) for i,_ in enumerate(custom_labels_list)]
+
+    iter_per_epoch = len(losses_list[0]) // num_epochs
+
+    plt.figure()
+    ax1 = plt.subplot(1, 1, 1)
+
+    for i, minibatch_loss_tensor in enumerate(losses_list):
+        ax1.plot(range(len(minibatch_loss_tensor)),
+                 (minibatch_loss_tensor),
+                  label=f'Minibatch Loss{custom_labels_list[i]}')
+        ax1.set_xlabel('Iterations')
+        ax1.set_ylabel('Loss')
+
+        ax1.plot(np.convolve(minibatch_loss_tensor,
+                             np.ones(averaging_iterations,)/averaging_iterations,
+                             mode='valid'),
+                 color='black')
+
+    if len(losses_list[0]) < 1000:
+        num_losses = len(losses_list[0]) // 2
+    else:
+        num_losses = 1000
+    maxes = [np.max(losses_list[i][num_losses:]) for i,_ in enumerate(losses_list)]
+    ax1.set_ylim([0, np.max(maxes)*1.5])
+    ax1.legend()
+
+    ###################
+    # Set scond x-axis
+    ax2 = ax1.twiny()
+    newlabel = list(range(num_epochs+1))
+
+    newpos = [e*iter_per_epoch for e in newlabel]
+
+    ax2.set_xticks(newpos[::10])
+    ax2.set_xticklabels(newlabel[::10])
+
+    ax2.xaxis.set_ticks_position('bottom')
+    ax2.xaxis.set_label_position('bottom')
+    ax2.spines['bottom'].set_position(('outward', 45))
+    ax2.set_xlabel('Epochs')
+    ax2.set_xlim(ax1.get_xlim())
+    ###################
+
+    plt.tight_layout()
+
+def plot_generated_images(data_loader, model, device,
+                          unnormalizer=None,
+                          figsize=(20, 2.5), n_images=15, modeltype='autoencoder'):
+
+    fig, axes = plt.subplots(nrows=2, ncols=n_images,
+                             sharex=True, sharey=True, figsize=figsize)
+
+    for batch_idx, (features, _) in enumerate(data_loader):
+
+        features = features.to(device)
+
+        color_channels = features.shape[1]
+        image_height = features.shape[2]
+        image_width = features.shape[3]
+
+        with torch.no_grad():
+            if modeltype == 'autoencoder':
+                decoded_images = model(features)[:n_images]
+            elif modeltype == 'VAE':
+                encoded, z_mean, z_log_var, decoded_images = model(features)[:n_images]
+            else:
+                raise ValueError('`modeltype` not supported')
+
+        orig_images = features[:n_images]
+        break
+
+    for i in range(n_images):
+        for ax, img in zip(axes, [orig_images, decoded_images]):
+            curr_img = img[i].detach().to(torch.device('cpu'))
+            if unnormalizer is not None:
+                curr_img = unnormalizer(curr_img)
+
+            if color_channels > 1:
+                curr_img = np.transpose(curr_img, (1, 2, 0))
+                ax[i].imshow(curr_img)
+            else:
+                ax[i].imshow(curr_img.view((image_height, image_width)), cmap='binary')
+
+##########################
+### SETTINGS
+##########################
+
+# Device
+CUDA_DEVICE_NUM = 2
+#DEVICE = torch.device(f'cuda:{CUDA_DEVICE_NUM}' if torch.cuda.is_available() else 'cpu')
+DEVICE = 'cuda'
+print('Device:', DEVICE)
+
+# Hyperparameters
+RANDOM_SEED = 42
+GENERATOR_LEARNING_RATE = 0.0002
+DISCRIMINATOR_LEARNING_RATE = 0.0002
+
+NUM_EPOCHS = 2
+BATCH_SIZE = 128
+
+IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 64, 64, 3
+
+set_deterministic
+set_all_seeds(RANDOM_SEED)
+
+##########################
+### Dataset
+##########################
+
+
 custom_transforms = torchvision.transforms.Compose([
-    torchvision.transforms.CenterCrop(64),
-    torchvision.transforms.Resize(64),
+    torchvision.transforms.CenterCrop((160, 160)),
+    torchvision.transforms.Resize([IMAGE_HEIGHT, IMAGE_WIDTH]),
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
 
 train_loader, valid_loader, test_loader = get_dataloaders_celeba(
-    batch_size=128,
+    batch_size=BATCH_SIZE,
     train_transforms=custom_transforms,
     test_transforms=custom_transforms,
     num_workers=4)
+
+
+# Checking the dataset
+print('Training Set:\n')
+for images, labels in train_loader:
+    print('Image batch dimensions:', images.size())
+    print('Image label dimensions:', labels.size())
+    #print(labels[:10])
+    break
+
+# Checking the dataset
+print('\nValidation Set:')
+for images, labels in valid_loader:
+    print('Image batch dimensions:', images.size())
+    print('Image label dimensions:', labels.size())
+    #print(labels[:10])
+    break
+
+# Checking the dataset
+print('\nTesting Set:')
+for images, labels in test_loader:
+    print('Image batch dimensions:', images.size())
+    print('Image label dimensions:', labels.size())
+    #print(labels[:10])
+    break
+
+plt.figure(figsize=(8, 8))
+plt.axis("off")
+plt.title("Training Images")
+plt.imshow(np.transpose(torchvision.utils.make_grid(images[:64],
+                                         padding=2, normalize=True),
+                        (1, 2, 0)))
