145b7c9c8c956470c908c07b555a62e5c6b53819
azatkariuly <azat.kariuly@gmail.com>
2022-09-29 13:37:33 +0900

    initial commit

--- all_in_one.py
+++ all_in_one.py
@@ -31,7 +31,7 @@ import os
 
 import time
 
-from quantization_modules import Conv2dLSQ, TransposeConv2dLSQ
+# from quantization_modules import Conv2dLSQ, TransposeConv2dLSQ
 
 nbits = 2
 
@@ -84,15 +84,15 @@ class Generator(nn.Module):
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
             # state size. (ngf*8) x 4 x 4
-            TransposeConv2dLSQ(ngf * 8, ngf * 4, 4, 2, 1, bias=False, nbits=nbits),
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
             # state size. (ngf*4) x 8 x 8
-            TransposeConv2dLSQ( ngf * 4, ngf * 2, 4, 2, 1, bias=False, nbits=nbits),
+            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
-            TransposeConv2dLSQ( ngf * 2, ngf, 4, 2, 1, bias=False, nbits=nbits),
+            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
             # state size. (ngf) x 32 x 32
@@ -444,10 +444,10 @@ for epoch in range(num_epochs):
                          errD.item(), errG.item(),fretchet_dist, best_res))
 
 plt.figure(figsize=(10,5))
-plt.title("Generator and Discriminator Loss During Training (Generator 2-bit)")
+plt.title("Generator and Discriminator Loss During Training")
 plt.plot(G_losses,label="G")
 plt.plot(D_losses,label="D")
 plt.xlabel("iterations")
 plt.ylabel("Loss")
 plt.legend()
-plt.savefig('g2b.png')
+plt.savefig('fp32.png')
