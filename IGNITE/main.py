f434652047ae13d22b6ef39fc9789f9df02d1db9
azatkariuly <azat.kariuly@gmail.com>
2022-11-07 01:40:21 +0900

    initial commit

--- IGNITE/main.py
+++ IGNITE/main.py
@@ -20,6 +20,8 @@ import ignite
 from ignite.engine import Engine, Events
 import ignite.distributed as idist
 
+from sagan_models import Generator, Discriminator
+
 
 ignite.utils.manual_seed(999)
 
@@ -70,36 +72,36 @@ train_dataloader = idist.auto_dataloader(
 
 latent_dim = 100
 
-class Generator3x64x64(nn.Module):
-    def __init__(self, latent_dim):
-        super(Generator3x64x64, self).__init__()
-        self.model = nn.Sequential(
-            nn.ConvTranspose2d(latent_dim, 512, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(512),
-            nn.ReLU(True),
-            # state size. 512 x 4 x 4
-            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.ReLU(True),
-            # state size. 256 x 8 x 8
-            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.ReLU(True),
-            # state size. 128 x 16 x 16
-            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(True),
-            # state size. 64 x 32 x 32
-            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),
-            nn.Tanh()
-            # final state size. 3 x 64 x 64
-        )
+# class Generator3x64x64(nn.Module):
+#     def __init__(self, latent_dim):
+#         super(Generator3x64x64, self).__init__()
+#         self.model = nn.Sequential(
+#             nn.ConvTranspose2d(latent_dim, 512, 4, 1, 0, bias=False),
+#             nn.BatchNorm2d(512),
+#             nn.ReLU(True),
+#             # state size. 512 x 4 x 4
+#             nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(256),
+#             nn.ReLU(True),
+#             # state size. 256 x 8 x 8
+#             nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(128),
+#             nn.ReLU(True),
+#             # state size. 128 x 16 x 16
+#             nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(64),
+#             nn.ReLU(True),
+#             # state size. 64 x 32 x 32
+#             nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),
+#             nn.Tanh()
+#             # final state size. 3 x 64 x 64
+#         )
+#
+#     def forward(self, x):
+#         x = self.model(x)
+#         return x
 
-    def forward(self, x):
-        x = self.model(x)
-        return x
-
-netG = idist.auto_model(Generator3x64x64(latent_dim))
+netG = idist.auto_model(Generator(batch_size=batch_size, z_dim=latent_dim))
 
 idist.device()
 
