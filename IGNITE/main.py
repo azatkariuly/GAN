726511dac6fb5895c6174776b0f97d43072e4eb4
azatkariuly <azat.kariuly@gmail.com>
2022-11-06 18:42:29 +0900

    initial commit

--- IGNITE/main.py
+++ IGNITE/main.py
@@ -160,13 +160,13 @@ def training_step(engine, data):
 
 trainer = Engine(training_step)
 
-def initialize_fn(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
+# def initialize_fn(m):
+#     classname = m.__class__.__name__
+#     if classname.find('Conv') != -1:
+#         nn.init.normal_(m.weight.data, 0.0, 0.02)
+#     elif classname.find('BatchNorm') != -1:
+#         nn.init.normal_(m.weight.data, 1.0, 0.02)
+#         nn.init.constant_(m.bias.data, 0)
 
 @trainer.on(Events.STARTED)
 def init_weights():
